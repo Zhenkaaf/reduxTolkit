bbbe5d0d80a5cf27a7d65b22aa81da863d67c52e
Zhenkaaf <zhenkaaf@gmail.com>
2023-05-03 12:47:09 +0300

    redux-toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,59 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import InputField from './components/InputField';
 import TodoList from './components/TodoList';
+import { addTodo} from './store/todoSlice';
+
 
 
 function App() {
 
 
-  const [todos, setTodos] = useState([]);
-  const [text, setText] = useState('');
-
-
-  const addTodo = () => {
-    if (text.trim().length) {
-      setTodos([
-        ...todos,
-        {
-          id: new Date().toISOString(),
-          text,
-          completed: false,
-        }
-      ]);
-      setText('');
-    }
-  };
-
-  const removeTodo = (todoId) => {
-    setTodos(todos.filter(todo => todo.id !== todoId));
-  };
-
-  const toggleTodoComplete = (todoId) => {
-    setTodos(todos.map(todo => {
-      if (todo.id !== todoId) return todo;
-      return {
-        ...todo,
-        completed: !todo.completed
+  /*   const [todos, setTodos] = useState([]);
+    
+  
+  
+    const addTodo = () => {
+      if (text.trim().length) {
+        setTodos([
+          ...todos,
+          {
+            id: new Date().toISOString(),
+            text,
+            completed: false,
+          }
+        ]);
+        setText('');
       }
-    }));
+    };
+  
+    const removeTodo = (todoId) => {
+      setTodos(todos.filter(todo => todo.id !== todoId));
+    };
+  
+    const toggleTodoComplete = (todoId) => {
+      setTodos(todos.map(todo => {
+        if (todo.id !== todoId) return todo;
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      }));
+    }; */
+  const [text, setText] = useState('');
+  const dispatch = useDispatch();
+  const addTask = () => {
+    dispatch(addTodo({ text }));
+    setText('');
   };
-
 
 
   return (
     <div className="App">
-      <InputField text={text} setText={setText} addTodo={addTodo} />
+      <InputField text={text} setText={setText} addTodo={addTask} />
 
-      <TodoList todos={todos} toggleTodoComplete={toggleTodoComplete} removeTodo={removeTodo}/>
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,15 @@
+import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 
 
 
-
-const TodoList = ({ todos, toggleTodoComplete, removeTodo }) => {
-
+const TodoList = () => {
+const todos = useSelector(state => state.todosRed.todos)
 
     return (
         <ul>
             {todos.map(todo => (
-                <TodoItem key={todo.id} {...todo} toggleTodoComplete={toggleTodoComplete} removeTodo={removeTodo} />
+                <TodoItem key={todo.id} {...todo} />
             ))}
         </ul>
     )
